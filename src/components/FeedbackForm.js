4ecf27d91f99d3f5371e77ecbdb69fc13c76d2b6
holtzmano <holtzmanoren99@gmail.com>
2023-07-20 19:10:42 +0300

    feedback done?

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,35 +1,101 @@
 import React, { useState } from 'react';
 import '../App.css';
+import { ref, push } from "firebase/database";
+import { database } from '../firebaseConfig';
 
 function FeedbackForm() {
     const [name, setName] = useState('');
     const [date, setDate] = useState('');
     const [feedback, setFeedback] = useState('');
+    const [ratingDesign, setRatingDesign] = useState(0);
+    const [ratingNavigation, setRatingNavigation] = useState(0);
+    const [ratingContent, setRatingContent] = useState(0);
+    const [ratingPerformance, setRatingPerformance] = useState(0);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log({name, date, feedback});
+    const resetForm = () => {
         setName('');
         setDate('');
         setFeedback('');
-    }
+        setRatingDesign(0);
+        setRatingNavigation(0);
+        setRatingContent(0);
+        setRatingPerformance(0);
+    };
+
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Push feedback to Firebase
+        const feedbackRef = ref(database, 'feedbacks');
+        push(feedbackRef, {
+            name,
+            date,
+            feedback,
+            ratingDesign,
+            ratingNavigation,
+            ratingContent,
+            ratingPerformance
+        })
+            .then(() => {
+                alert("Thanks for your feedback (:");
+                resetForm();
+            })
+            .catch(error => {
+                console.error("Error: ", error.message);
+                alert("Error details: " + error.message);
+            });
+    };
+
+    const currentRatings = [ratingDesign, ratingNavigation, ratingContent, ratingPerformance];
 
     return (
         <div className="feedback">
             <form onSubmit={handleSubmit}>
-                <h1>Give Us Your Feedback</h1>
+                <h1>Please Give Me Your Feedback</h1>
+
                 <label>
                     Name:
                     <input type="text" value={name} onChange={(e) => setName(e.target.value)} required/>
                 </label>
+
                 <label>
                     Date:
                     <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required/>
                 </label>
+
+                {["Design & Aesthetics", "Navigation", "Content Quality", "Performance & Speed"].map((category, idx) => {
+                    const setters = [setRatingDesign, setRatingNavigation, setRatingContent, setRatingPerformance];
+                    return (
+                        <div key={idx} className="rating-section">
+                            <p>Rate {category}:</p>
+                            {[...Array(5)].map((star, i) => {
+                                const ratingValue = i + 1;
+                                return (
+                                    <label key={i}>
+                                        <input
+                                            type="radio"
+                                            name={`rating${category.replace(/ & | /g, '')}`}
+                                            value={ratingValue}
+                                            onClick={() => setters[idx](ratingValue)}
+                                        />
+                                        <span className={`star ${ratingValue <= currentRatings[idx] ? 'active' : ''}`}>&#9733;</span>
+                                    </label>
+                                );
+                            })}
+
+                        </div>
+                    );
+                })}
+
+
+
                 <label>
                     Feedback:
                     <textarea rows="5" value={feedback} onChange={(e) => setFeedback(e.target.value)} required/>
                 </label>
+
                 <input type="submit" value="Submit" />
             </form>
         </div>
---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+import { getDatabase } from "firebase/database";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAVQKD8uz_WjfYNJgLUeQzwYL-hYxhY2L8",
+    authDomain: "orens-website.firebaseapp.com",
+    databaseURL: "https://orens-website-default-rtdb.firebaseio.com",
+    projectId: "orens-website",
+    storageBucket: "orens-website.appspot.com",
+    messagingSenderId: "902805241354",
+    appId: "1:902805241354:web:6178deeb598cf9cc9de9f3",
+    measurementId: "G-C7CTML1F93"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const database = getDatabase(app);
+
