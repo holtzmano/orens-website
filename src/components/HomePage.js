2c776d2706c5800210f947580058a1a16165d115
holtzmano <holtzmanoren99@gmail.com>
2023-07-28 16:01:27 +0300

    changed colors, buttons, added icons

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,6 +3,8 @@ import {Link} from "react-router-dom";
 import {useEffect} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
 
 
 function HomePage() {
@@ -11,15 +13,6 @@ function HomePage() {
     }, []);
     return (
         <div className="homepage">
-            <h1>Welcome to Oren's Website</h1>
-            <h2>Click on one of the buttons that you're interested in:</h2>
-            <Link to="/about"> <button className="button">About</button> </Link>
-            <a href={`${process.env.PUBLIC_URL}/Oren_Holtzman_CV.pdf`} target="_blank" rel="noopener noreferrer">
-                <button className="button">CV</button>
-            </a>
-            <Link to="/projects"> <button className="button">Projects</button> </Link>
-            <Link to="/feedback"> <button className="button">Feedback</button> </Link>
-
             <div className="social-icons">
                 <a href="https://www.linkedin.com/in/oren-holtzman-923b04206/" target="_blank" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faLinkedin} size="5x"/>
@@ -32,6 +25,14 @@ function HomePage() {
                 </a>
             </div>
 
+            <h1>Welcome to Oren's Website</h1>
+            <h2>Click on one of the buttons that you're interested in:</h2>
+            <Link to="/about"> <button className="button">About</button> </Link>
+            <a href={`${process.env.PUBLIC_URL}/Oren_Holtzman_CV.pdf`} target="_blank" rel="noopener noreferrer">
+                <button className="button">CV</button>
+            </a>
+            <Link to="/projects"> <button className="button">Projects</button> </Link>
+            <Link to="/feedback"> <button className="button">Feedback</button> </Link>
             <div className="developer-credit">Developed by Oren Holtzman</div> {/* Added line */}
         </div>
     );
